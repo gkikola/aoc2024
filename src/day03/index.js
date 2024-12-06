5a1c0ca9086c61218f99cd0b07e74da10b0efa59
Greg Kikola <gkikola@gmail.com>
2024-12-05 20:06:35 -0500

    Add solution to Day 3 Part 2

--- src/day03/index.js
+++ src/day03/index.js
@@ -1,12 +1,31 @@
 export default function run(input) {
-  const matches = input.matchAll(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/g);
+  const matches = input.matchAll(
+    /((mul\(([0-9]{1,3}),([0-9]{1,3})\))|do\(\)|don't\(\))/g,
+  );
   let sum = 0;
+  let conditionalSum = 0;
+  let mulEnabled = true;
 
   for (const match of matches) {
-    const value1 = Number.parseInt(match[1], 10);
-    const value2 = Number.parseInt(match[2], 10);
-    sum += value1 * value2;
+    switch (match[1].slice(0, 3)) {
+      case 'mul':
+        {
+          const value1 = Number.parseInt(match[3], 10);
+          const value2 = Number.parseInt(match[4], 10);
+          sum += value1 * value2;
+          if (mulEnabled) conditionalSum += value1 * value2;
+        }
+        break;
+      case 'do(':
+        mulEnabled = true;
+        break;
+      case 'don':
+        mulEnabled = false;
+        break;
+      default:
+        break;
+    }
   }
 
-  return sum;
+  return [sum, conditionalSum];
 }
