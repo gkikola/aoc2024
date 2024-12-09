4510461a55f8af0a411bb4d6a3d7485f82628033
Greg Kikola <gkikola@gmail.com>
2024-12-08 19:19:22 -0500

    Add solution for Day 8 Part 2

--- src/day08/index.js
+++ src/day08/index.js
@@ -44,36 +44,83 @@ class City {
     }
   }
 
-  #addAntinodes(antinodes, antennas) {
+  #getAntinodePositions(antenna1, antenna2, distanceMultiple, deltaMultiple) {
+    const [firstX, firstY] = this.#indexToCoords(antenna1);
+    const [secondX, secondY] = this.#indexToCoords(antenna2);
+
+    const deltaX = secondX - firstX;
+    const deltaY = secondY - firstY;
+
+    const getOffset = (multiple) => [
+      firstX + deltaX * multiple,
+      firstY + deltaY * multiple,
+    ];
+
+    const start = getOffset(distanceMultiple);
+    if (!this.#isPositionInBounds(start)) return [];
+
+    const positions = [start];
+    if (deltaMultiple === 0) return positions;
+
+    for (
+      let multiple = distanceMultiple + deltaMultiple;
+      ;
+      multiple += deltaMultiple
+    ) {
+      const position = getOffset(multiple);
+      if (this.#isPositionInBounds(position)) positions.push(position);
+      else break;
+    }
+
+    return positions;
+  }
+
+  #addAntinodes(antinodes, antennas, withResonance = false) {
     if (antennas.length < 2) return;
 
-    const [firstX, firstY] = this.#indexToCoords(antennas[0]);
+    const [antenna1] = antennas;
     const rest = antennas.slice(1);
 
-    rest.forEach((secondAntenna) => {
-      const [secondX, secondY] = this.#indexToCoords(secondAntenna);
+    rest.forEach((antenna2) => {
+      const newPositions = [];
 
-      const deltaX = secondX - firstX;
-      const deltaY = secondY - firstY;
+      if (withResonance) {
+        // Start at first antenna and go backwards
+        // Then start at second antenna and go forwards
+        newPositions.push(
+          ...this.#getAntinodePositions(antenna1, antenna2, 0, -1),
+        );
+        newPositions.push(
+          ...this.#getAntinodePositions(antenna1, antenna2, 1, 1),
+        );
+      } else {
+        // Two antinode locations are one offset before first antenna
+        // and two offsets after first antenna
+        newPositions.push(
+          ...this.#getAntinodePositions(antenna1, antenna2, -1, 0),
+        );
+        newPositions.push(
+          ...this.#getAntinodePositions(antenna1, antenna2, 2, 0),
+        );
+      }
 
-      const location1 = [firstX - deltaX, firstY - deltaY];
-      const location2 = [secondX + deltaX, secondY + deltaY];
-
-      [location1, location2].forEach((location) => {
-        if (this.#isPositionInBounds(location)) {
-          antinodes.add(this.#coordsToIndex(location));
-        }
+      newPositions.forEach((position) => {
+        antinodes.add(this.#coordsToIndex(position));
       });
     });
 
-    this.#addAntinodes(antinodes, rest);
+    this.#addAntinodes(antinodes, rest, withResonance);
   }
 
-  countAntinodes() {
+  countAntinodes(withResonance = false) {
     const antinodes = new Set();
 
     for (const frequency of this.#antennas.keys()) {
-      this.#addAntinodes(antinodes, this.#antennas.get(frequency));
+      this.#addAntinodes(
+        antinodes,
+        this.#antennas.get(frequency),
+        withResonance,
+      );
     }
 
     return antinodes.size;
@@ -82,5 +129,5 @@ class City {
 
 export default function run(input) {
   const city = new City(input);
-  return city.countAntinodes();
+  return [city.countAntinodes(), city.countAntinodes(true)];
 }
