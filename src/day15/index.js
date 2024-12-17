c08c40e21ad0f0cc2717d23d5abe94b99dcbba36
Greg Kikola <gkikola@gmail.com>
2024-12-16 20:51:14 -0500

    Add solution for Day 15 Part 2

--- src/day15/index.js
+++ src/day15/index.js
@@ -51,29 +51,52 @@ class Warehouse {
     this.#cells[this.#coordsToIndex(x, y)] = value;
   }
 
+  #isPushable(x, y, deltaX, deltaY) {
+    const curCell = this.getCell(x, y);
+    if (curCell === '#' || !this.#isPositionInBounds(x, y)) return false;
+    if (curCell === '.') return true;
+
+    // If the box is narrow or being moved horizontally...
+    if (curCell === 'O' || deltaY === 0) {
+      return this.#isPushable(x + deltaX, y + deltaY, deltaX, deltaY);
+    }
+
+    // Otherwise, the box is wide and being moved vertically
+    const leftX = (curCell === '[' ? x : x - 1) + deltaX;
+    const rightX = (curCell === '[' ? x + 1 : x) + deltaX;
+    return (
+      this.#isPushable(leftX, y + deltaY, deltaX, deltaY) &&
+      this.#isPushable(rightX, y + deltaY, deltaX, deltaY)
+    );
+  }
+
   // Returns true if the box was successfully pushed
   #pushBox(x, y, deltaX, deltaY) {
-    if (this.getCell(x, y) !== 'O') return false;
+    if (!this.#isPushable(x, y, deltaX, deltaY)) return false;
 
+    const curCell = this.getCell(x, y);
     const targetX = x + deltaX;
     const targetY = y + deltaY;
-    if (!this.#isPositionInBounds(targetX, targetY)) return false;
 
-    const targetCell = this.getCell(targetX, targetY);
-    if (targetCell === '.') {
+    if (deltaY !== 0 && (curCell === '[' || curCell === ']')) {
+      const leftX = (curCell === '[' ? x : x - 1) + deltaX;
+      const rightX = (curCell === '[' ? x + 1 : x) + deltaX;
+
+      // Clear space for left and right halves of the box
+      this.#pushBox(leftX, targetY, deltaX, deltaY);
+      this.#pushBox(rightX, targetY, deltaX, deltaY);
+
+      this.#setCell(leftX, targetY, '[');
+      this.#setCell(rightX, targetY, ']');
+      this.#setCell(leftX - deltaX, y, '.');
+      this.#setCell(rightX - deltaX, y, '.');
+    } else if (curCell === 'O' || curCell === '[' || curCell === ']') {
+      this.#pushBox(targetX, targetY, deltaX, deltaY);
+      this.#setCell(targetX, targetY, curCell);
       this.#setCell(x, y, '.');
-      this.#setCell(targetX, targetY, 'O');
-      return true;
     }
 
-    if (targetCell === 'O') {
-      if (!this.#pushBox(targetX, targetY, deltaX, deltaY)) return false;
-      this.#setCell(x, y, '.');
-      this.#setCell(targetX, targetY, 'O');
-      return true;
-    }
-
-    return false;
+    return true;
   }
 
   #moveRobot(deltaX, deltaY) {
@@ -89,7 +112,7 @@ class Warehouse {
       return true;
     }
 
-    if (targetCell === 'O') {
+    if (targetCell === 'O' || targetCell === '[' || targetCell === ']') {
       if (!this.#pushBox(targetX, targetY, deltaX, deltaY)) return false;
       this.#robot = this.#coordsToIndex(targetX, targetY);
       return true;
@@ -130,18 +153,51 @@ class Warehouse {
 
   get gpsSum() {
     return this.#cells.reduce((sum, value, index) => {
-      if (value === 'O') {
+      if (value === 'O' || value === '[') {
         return sum + Warehouse.getGpsCoordinate(...this.#indexToCoords(index));
       }
 
       return sum;
     }, 0);
   }
+
+  toString() {
+    let result = '';
+    for (let y = 0; y < this.#height; y++) {
+      if (y > 0) result += '\n';
+
+      for (let x = 0; x < this.#width; x++) {
+        const index = this.#coordsToIndex(x, y);
+        if (this.#robot === index) result += '@';
+        else result += this.#cells[index];
+      }
+    }
+    return result;
+  }
 }
 
 export default function run(input) {
-  const warehouse = new Warehouse(input);
-  warehouse.run();
+  const stdWarehouse = new Warehouse(input);
+  stdWarehouse.run();
+  const stdGpsSum = stdWarehouse.gpsSum;
 
-  return warehouse.gpsSum;
+  const wideInput = input.replaceAll(/#|O|\.|@/g, (match) => {
+    switch (match) {
+      case '#':
+        return '##';
+      case 'O':
+        return '[]';
+      case '.':
+        return '..';
+      case '@':
+        return '@.';
+      default:
+        return `${match}${match}`;
+    }
+  });
+  const wideWarehouse = new Warehouse(wideInput);
+  wideWarehouse.run();
+  const wideGpsSum = wideWarehouse.gpsSum;
+
+  return [stdGpsSum, wideGpsSum];
 }
