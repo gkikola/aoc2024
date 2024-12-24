75880d340f86ee184db06fd332b3de78e32bf466
Greg Kikola <gkikola@gmail.com>
2024-12-24 11:38:07 -0500

    Add solution for Day 19 Part 2

--- src/day19/index.js
+++ src/day19/index.js
@@ -3,6 +3,9 @@ class DesignCounter {
   #designs;
   #cache;
 
+  #possibleCount;
+  #arrangementCount;
+
   constructor(towelData) {
     const [patternLine, designList] = towelData.split('\n\n');
     this.#patterns = new Set(patternLine.split(', '));
@@ -10,36 +13,55 @@ class DesignCounter {
       .split('\n')
       .filter((design) => design.length > 0);
     this.#cache = new Map();
+
+    this.#possibleCount = null;
+    this.#arrangementCount = null;
   }
 
-  testDesign(design) {
-    if (design.length === 0) return true;
+  countArrangements(design) {
     if (this.#cache.has(design)) return this.#cache.get(design);
 
+    let count = 0;
     for (let i = 1; i <= design.length; i++) {
       // If the start of the design matches, recursively test the rest of it
-      if (
-        this.#patterns.has(design.slice(0, i)) &&
-        this.testDesign(design.slice(i))
-      ) {
-        this.#cache.set(design, true);
-        return true;
+      if (this.#patterns.has(design.slice(0, i))) {
+        if (i === design.length) {
+          count++;
+        } else {
+          const substrCount = this.countArrangements(design.slice(i));
+          if (substrCount > 0) count += substrCount;
+        }
       }
     }
 
-    this.#cache.set(design, false);
-    return false;
+    this.#cache.set(design, count);
+    return count;
+  }
+
+  #updateCounts() {
+    this.#possibleCount = 0;
+    this.#arrangementCount = 0;
+    this.#designs.forEach((design) => {
+      const count = this.countArrangements(design);
+      if (count > 0) this.#possibleCount++;
+      this.#arrangementCount += count;
+    });
   }
 
   countPossibleDesigns() {
-    return this.#designs.reduce((count, design) => {
-      if (this.testDesign(design)) return count + 1;
-      return count;
-    }, 0);
+    if (this.#possibleCount == null) this.#updateCounts();
+
+    return this.#possibleCount;
+  }
+
+  countTotalArrangements() {
+    if (this.#arrangementCount == null) this.#updateCounts();
+
+    return this.#arrangementCount;
   }
 }
 
 export default function run(input) {
   const counter = new DesignCounter(input);
-  return counter.countPossibleDesigns();
+  return [counter.countPossibleDesigns(), counter.countTotalArrangements()];
 }
